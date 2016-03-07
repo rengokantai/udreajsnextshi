eb739ad15165d033f2004a518e893f1d2762da63
Hernan Y.Ke <yuriqiao@gmail.com>
2016-03-07 14:47:55 -0500

    Using function to create observable/subscribe

--- srcserver/rxex2.js
+++ srcserver/rxex2.js
@@ -1,3 +1,30 @@
 /**
  * Created by Hernan Y.Ke on 2016/3/7.
  */
+import Rx from 'rxjs/Rx';
+function createInt(time){
+    return new Rx.Observable(observer=>{
+        let i =0;
+        var timer = setInterval(()=>{
+            observer.next(i++);
+
+            if (i==10)
+            clearInterval(timer);
+        },time)
+    })
+}
+
+function createSub(t){
+    return{
+        next(item){console.log(`${t}${item}`)},
+        error(error){
+            console.log(`${error}`)
+        },
+        complete(){
+            console.log(`complete`)
+        }
+    }
+}
+
+const sec = createInt(1000);
+sec.subscribe(createSub("newsub"));
